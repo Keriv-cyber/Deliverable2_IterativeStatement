1d94d1752f32d2ee026ae5d8dafad91886be4e0d
Keri Vollenweider <keriv@usf.edu>
2021-01-28 16:15:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Deliverable2_IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number;
+
+
+            Console.WriteLine("Please enter an interger value between 1 and 100.");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            
+            //this will be the interative statement for deliverable 2
+            while (count <= number)
+            {
+                Console.WriteLine("You have entered #. This is the current interger value in the loop: " + number);
+                count = count + 1;
+            }
+          
+            
+        }
+    }
+}
